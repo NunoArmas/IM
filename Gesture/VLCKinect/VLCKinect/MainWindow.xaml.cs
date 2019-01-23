d3a23174ea43423f15dbbd6927cd2f2c7d053032
Nuno <nuno.abarbosa@hotmail.com>
2019-01-23 21:19:47 +0000

    communication name

--- Gesture/VLCKinect/VLCKinect/MainWindow.xaml.cs
+++ Gesture/VLCKinect/VLCKinect/MainWindow.xaml.cs
@@ -67,7 +67,7 @@ namespace VLCKinect
             
 
             //init LifeCycleEvents..
-            lce = new LifeCycleEvents("ASR", "FUSION", "gesture-1", "gesture", "command"); // LifeCycleEvents(string source, string target, string id, string medium, string mode)
+            lce = new LifeCycleEvents("KINECT", "FUSION", "gesture-1", "gesture", "command"); // LifeCycleEvents(string source, string target, string id, string medium, string mode)
             //mmic = new MmiCommunication("localhost",9876,"User1", "ASR");  //PORT TO FUSION - uncomment this line to work with fusion later
             mmic = new MmiCommunication("localhost", 8000, "User2", "ASR"); // MmiCommunication(string IMhost, int portIM, string UserOD, string thisModalityName)
 
